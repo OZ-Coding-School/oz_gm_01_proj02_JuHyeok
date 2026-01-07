070fd69bb1c7ec117145d499e8676ed58df924cc
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2026-01-07 19:44:33 +0900

    feat : 경로 노드 스크립트 작성

--- Assets/Scripts/Objects/Node.cs
+++ Assets/Scripts/Objects/Node.cs
@@ -8,8 +8,9 @@ public class Node : MonoBehaviour
     public bool isWalkable = true;
     public float stepCost = 1f;
 
-    [Header("연결된 노드")]
+    [Header("노드 연결")]
     public List<Node> neighborNodes = new List<Node>();
+    public float scanDistance = 1.05f;
 
     [HideInInspector]
     public Node illusionNeighbor;           // 착시로 연결되는 노드
@@ -17,6 +18,49 @@ public class Node : MonoBehaviour
     // 캐릭터가 설 노드 중앙값
     public Vector3 walkTarget => transform.position + Vector3.up * 0.5f;
 
+    /// <summary>
+    /// 주변 노드 탐색, 이웃 노드 리스트에 할당
+    /// </summary>
+    [ContextMenu("Scan Neighbors")]
+    public void ScanNeighbors()
+    {
+        neighborNodes.Clear();
+
+        Vector3[] directions = 
+            { Vector3.up, 
+            Vector3.down, 
+            Vector3.left, 
+            Vector3.right, 
+            Vector3.forward, 
+            Vector3.back 
+        };
+
+        foreach (Vector3 dir in directions)
+        {
+            if (Physics.Raycast(transform.position + Vector3.up * 0.5f, dir, out RaycastHit hit, scanDistance))
+            {
+                Node neighborNode = hit.collider.GetComponent<Node>();
+
+                if (neighborNode != null && neighborNode != this)
+                {
+                    neighborNodes.Add(neighborNode);
+                }
+            }
+        }
+    }
+
+    // 노드 연결 확인 기즈모
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+
+        foreach (var neighbor in neighborNodes)
+        {
+            if (neighbor != null)
+                Gizmos.DrawLine(transform.position + Vector3.up * 0.5f, neighbor.transform.position + Vector3.up * 0.5f);
+        }
+    }
+    // 이동 가능 여부 확인 기즈모
     private void OnDrawGizmos()
     {
         Gizmos.color = isWalkable ? Color.green : Color.red;
