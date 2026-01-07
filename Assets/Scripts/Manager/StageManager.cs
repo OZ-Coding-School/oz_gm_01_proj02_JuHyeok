761d3187fcc95dd0344f630bc190a9ddfbb7bb25
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2026-01-07 19:43:51 +0900

    feat : 노드 전체 연결용 스크립트 작성

---
+++ Assets/Scripts/Manager/StageManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    [ContextMenu("Scan All Nodes in Scene")]
+    public void ScanAll()
+    {
+        Node[] allNodes = FindObjectsOfType<Node>();
+        foreach(Node node in allNodes)
+        {
+            node.ScanNeighbors();
+        }
+    }
+}
