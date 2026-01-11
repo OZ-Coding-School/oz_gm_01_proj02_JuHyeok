5f20e5a534a5d4e85bad8cb4d82edbfa6bdbcaad
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2026-01-12 03:56:26 +0900

    feat : 게임 매니저 클래스 작성

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Singleton<GameManager>
+{
+    // 클릭 프리팹
+    public Indicator IndicatorPrefab;
+
+    public override void Awake()
+    {
+        SetPool();
+        SetIllusion();
+
+        gameObject.AddComponent<StageManager>();
+        transform.SetParent(GameObject.Find("@Managers").transform);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit(); // 어플리케이션 종료
+#endif
+    }
+
+    public void SetPool()
+    {
+        Managers.Pool.CreatePool(IndicatorPrefab, 5);
+    }
+
+    public void RemovePool()
+    {
+        Managers.Pool.RemovePool(IndicatorPrefab);
+    }
+
+    public void SetIllusion(float distance = 50.0f, int count = 2)
+    {
+        Managers.Illusion.Init(distance, count);
+    }
+}
