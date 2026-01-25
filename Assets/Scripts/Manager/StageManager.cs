7629e40c8755220c8065aa1be4670e7a625ce063
JuHyeok <106865204+JHJuHyeok@users.noreply.github.com>
2026-01-25 18:45:30 +0900

    fix : StageManager 코드 정리

--- Assets/Scripts/Manager/StageManager.cs
+++ Assets/Scripts/Manager/StageManager.cs
@@ -5,25 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class StageManager : Singleton<StageManager>
 {
-    public override void Awake()
-    {
-        base.Awake();
-
-        SceneManager.sceneLoaded += OnSceneLoaded;
-        SceneManager.sceneUnloaded += OnSceneUnLoaded;
-    }
-
-    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        
-    }
-
-    private void OnSceneUnLoaded(Scene scene)
-    {
-
-    }
-
-
     [ContextMenu("Scan All Nodes in Scene")]
     public void ScanAll()
     {
